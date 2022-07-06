2eceb90a51ec892c0c2646607b853dfb209df7db
Argoth <argoth@MacBook-Pro-di-Argoth.local>
2022-07-06 18:12:15 +0200

    components-04

---
+++ react-exercises/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import React, { StrictMode } from "react";
+import { App } from "./App";
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
