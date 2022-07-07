88cec15e105733be8a77c89e4839afebbebd76e2
Argoth <argoth@MacBook-Pro-di-Argoth.local>
2022-07-07 19:03:19 +0200

    props-05

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -4,13 +4,20 @@ import { Welcome } from "./Welcome";
 
 const userName = <strong> Vincenzo</strong>;
 
-export class App extends React.Component {
-    render(){
-        return (
-            <div>
-                <Hello/>
-                <Welcome name={userName} age={36}/>
-            </div>
-        )
+export class Age extends React.Component {
+    render() {
+      return <p>Your age is {this.props.age}</p>;
     }
+  }
+
+  
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+        <Welcome name={userName} age={36} />
+      </div>
+    );
+  }
 }
--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -1,11 +1,13 @@
 import React from "react";
+import {Age} from "./App";
+
 
 export class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <p>Welcome,{this.props.name}!</p>
-        <p>Your age is {this.props.age}</p>
+        <p>Welcome, {this.props.name}!</p>
+        <Age />
       </div>
     );
   }
