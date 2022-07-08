2666814336ad5f5229ae96413ddaa27aca8d762e
Argoth <argoth@MacBook-Pro-di-Argoth.local>
2022-07-08 14:53:31 +0200

    conditional-rendering-03

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -8,7 +8,7 @@ export class Age extends React.Component {
   render() {
     return (
         <div>
-            {this.props.age && <p>Your age is {this.props.age}</p>}
+            {this.props.age >= 18 && this.props.age <= 65 && <p>Your age is {this.props.age}</p>}
         </div>
     );
   }
