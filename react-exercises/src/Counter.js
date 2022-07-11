1a6a8286d588df75d5f20d4e4becda6c834baa46
Argoth <argoth@MacBook-Pro-di-Argoth.local>
2022-07-11 19:39:55 +0200

    state-04

--- react-exercises/src/Counter.js
+++ react-exercises/src/Counter.js
@@ -19,6 +19,13 @@ export class Counter extends React.Component {
 }
 
   render() {
-    return <h1>Count: {this.state.count}</h1>;
+    return <CounterDisplay count={this.state.count}/>;
   }
+}
+
+
+export class CounterDisplay extends React.Component {
+    render(){
+        return <h1>Count: {this.props.count}</h1>
+    }
 }
