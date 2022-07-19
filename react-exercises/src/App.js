dfa1932dfa2b72dd6ff179e275a653abe762f556
Argoth <argoth@MacBook-Pro-di-Argoth.local>
2022-07-19 10:21:54 +0200

    composition-01

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,28 +1,29 @@
 import React from "react";
-// import { Hello } from "./Hello";
-// import { Welcome } from "./Welcome";
-// import {ClickTracker,ClickCounter,Counter} from "./Counter";
-import {Login} from "./Forms"
+import { Hello } from "./Hello";
+import { Welcome } from "./Welcome";
+import {ClickTracker,ClickCounter,Counter} from "./Counter";
+// import {Login} from "./Forms"
 // import {InteractiveWelcome} from "./Forms"
 // import {UncontrolledLogin} from './UncontrolledLogin'
 // import { TodoList } from "./TodoList";
+import {Container} from "./Container"
 
 
 export class App extends React.Component {
   render() {
     return (
-      <div>
-        {/* <Hello /> */}
-        {/* <Welcome name='John' age={36}/> */}
-        {/* <Counter initialValue={0} incrementBy={1} interval={1000}/>
+        <Container>
+        <Hello />
+        <Welcome name='John' age={36}/>
+        <Counter initialValue={0} incrementBy={1} interval={1000}/>
         <ClickCounter />
         <ClickTracker />
-        <InteractiveWelcome /> */}
-        <Login />
+        {/* <InteractiveWelcome /> */}
+        {/* <Login /> */}
         {/* <br/>
         <UncontrolledLogin /> */}
         {/* <TodoList /> */}
-      </div>
+        </Container>
     );
   }
 }
