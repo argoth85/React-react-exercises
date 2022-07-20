dd3ea7d997936b5b82641290f767b979af43c65f
Argoth <argoth@MacBook-Pro-di-Argoth.local>
2022-07-20 14:06:10 +0200

    context-02

--- react-exercises/src/DisplayLanguage.js
+++ react-exercises/src/DisplayLanguage.js
@@ -1,13 +1,22 @@
 import { createContext } from "react";
 import React from "react";
 
-export const LanguageContext = createContext('en');
-
+export const LanguageContext = createContext("en");
 
 export class DisplayLanguage extends React.Component {
-    render(){
-        return (
-            <div></div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <LanguageContext.Consumer>
+          {(language) => {
+            return (
+              <div>
+                <h1>{language}</h1>
+              </div>
+            );
+          }}
+        </LanguageContext.Consumer>
+      </div>
+    );
+  }
 }
