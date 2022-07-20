bc92623f8332d067ba5dd974793da0f839dac299
Argoth <argoth@MacBook-Pro-di-Argoth.local>
2022-07-20 11:43:04 +0200

    render-props-01

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,28 +1,43 @@
 import React from "react";
-import { Hello } from "./Hello";
-import { Welcome } from "./Welcome";
-import {ClickTracker,ClickCounter,Counter} from "./Counter";
+// import { Hello } from "./Hello";
+// import { Welcome } from "./Welcome";
+// import {ClickTracker,ClickCounter,Counter} from "./Counter";
 // import {Login} from "./Forms"
 // import {InteractiveWelcome} from "./Forms"
 // import {UncontrolledLogin} from './UncontrolledLogin'
-// import { TodoList } from "./TodoList";
+import { TodoList } from "./TodoList";
 import {Container} from "./Container"
+
 
 
 export class App extends React.Component {
   render() {
     return (
-        <Container title="This is a Title">
-        <Hello />
+        <Container title="To Do list:">
+        {/* <Hello />
         <Welcome name='John' age={36}/>
         <Counter initialValue={0} incrementBy={1} interval={1000}/>
         <ClickCounter />
-        <ClickTracker />
+        <ClickTracker /> */}
         {/* <InteractiveWelcome /> */}
         {/* <Login /> */}
         {/* <br/>
         <UncontrolledLogin /> */}
-        {/* <TodoList /> */}
+        <TodoList
+          render = {(items) => {
+            return (
+              <div>
+              <ul>
+                    {items.map((item, index) =><li key={item + index}>{item}
+                    <button className="button-remove-item" onClick={() => {items.splice(index, 1);
+                    this.setState({ 
+                            items: items });}}>Remove Item</button>
+                    </li>)}
+                </ul>
+                </div>
+            )
+          }}
+          ></TodoList>
         </Container>
     );
   }
--- react-exercises/src/TodoList.js
+++ react-exercises/src/TodoList.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 
-
 export class TodoList extends React.Component {
 
     state = {
@@ -33,18 +32,64 @@ export class TodoList extends React.Component {
 
     render() {
         return (
-            <div>
-                <ul>
-                    {this.state.items.map((item, index) =><li key={item + index}>{item}
-                    <button onClick={() => {this.state.items.splice(index, 1);
-                    this.setState({ 
-                            items: this.state.items });}}>Remove Item</button>
-                    </li>)}
-                </ul>
-                <input type="text" name="username" placeholder="Insert a name" onChange={this.handlerInputChange} value={this.state.inputValue}></input>
-                <button onClick={this.handlerAdd} disabled={!this.state.inputValue}>Add!</button>
-                <button onClick={this.resetItems}>Reset</button>
+            <div className="todo-list">
+                {this.props.render(this.state.items)}
+                <input className="input-item" type="text" name="username" placeholder="Insert a name" onChange={this.handlerInputChange} value={this.state.inputValue}></input>
+                <button className="add-btn" onClick={this.handlerAdd} disabled={!this.state.inputValue}>Add!</button>
+                <button className="button-remove-item" onClick={this.resetItems}>Reset</button>
             </div>
         )
     }
 }
+
+
+
+
+
+
+// export class TodoList extends React.Component {
+
+//     state = {
+//         items: ['Donatello', 'Michelangelo', 'Raffaello', 'Leonardo'],
+//         inputValue: ''
+//     };
+
+
+
+//     handlerAdd = () => {
+//         this.setState({
+//         items: [...this.state.items, this.state.inputValue],
+//         inputValue: ''
+//     });
+//     }
+    
+//     handlerInputChange = (event) => {
+//         this.setState({
+//             inputValue: event.target.value
+//         });
+//     }
+
+//     resetItems = () => {
+//         this.setState ({
+//             items: []
+//         })
+//     }
+
+
+//     render() {
+//         return (
+//             <div className="todo-list">
+//                 <ul>
+//                     {this.state.items.map((item, index) =><li key={item + index}>{item}
+//                     <button className="button-remove-item" onClick={() => {this.state.items.splice(index, 1);
+//                     this.setState({ 
+//                             items: this.state.items });}}>Remove Item</button>
+//                     </li>)}
+//                 </ul>
+//                 <input className="input-item" type="text" name="username" placeholder="Insert a name" onChange={this.handlerInputChange} value={this.state.inputValue}></input>
+//                 <button className="add-btn" onClick={this.handlerAdd} disabled={!this.state.inputValue}>Add!</button>
+//                 <button className="button-remove-item" onClick={this.resetItems}>Reset</button>
+//             </div>
+//         )
+//     }
+// }
