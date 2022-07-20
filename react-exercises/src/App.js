9ef4cb3576f8cefd347f7c222b185de0b724284f
Argoth <argoth@MacBook-Pro-di-Argoth.local>
2022-07-20 14:00:07 +0200

    context-01

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,44 +1,29 @@
 import React from "react";
-// import { Hello } from "./Hello";
-// import { Welcome } from "./Welcome";
-// import {ClickTracker,ClickCounter,Counter} from "./Counter";
-// import {Login} from "./Forms"
-// import {InteractiveWelcome} from "./Forms"
-// import {UncontrolledLogin} from './UncontrolledLogin'
-import { TodoList } from "./TodoList";
-import {Container} from "./Container"
-
-
+import { Container } from "./Container";
+import { DisplayLanguage, LanguageContext } from "./DisplayLanguage";
 
 export class App extends React.Component {
+  state = {
+    language: "en",
+  };
+
+  handleLanguageChange = (event) => {
+    this.setState({
+      language: event.target.value,
+    });
+  };
+
   render() {
     return (
+      <LanguageContext.Provider value={this.state.language}>
         <Container title="To Do list:">
-        {/* <Hello />
-        <Welcome name='John' age={36}/>
-        <Counter initialValue={0} incrementBy={1} interval={1000}/>
-        <ClickCounter />
-        <ClickTracker /> */}
-        {/* <InteractiveWelcome /> */}
-        {/* <Login /> */}
-        {/* <br/>
-        <UncontrolledLogin /> */}
-        <TodoList
-          render = {(items) => {
-            return (
-              <div>
-              <ul>
-                    {items.map((item, index) =><li key={item + index}>{item}
-                    <button className="button-remove-item" onClick={() => {items.splice(index, 1);
-                    this.setState({ 
-                            items: items });}}>Remove Item</button>
-                    </li>)}
-                </ul>
-                </div>
-            )
-          }}
-          ></TodoList>
+          <select value={this.state.language} onChange={this.handleLanguageChange}>
+            <option value="en">Inglese</option>
+            <option value="it">Italiano</option>
+          </select>
+          <DisplayLanguage />
         </Container>
+      </LanguageContext.Provider>
     );
   }
 }
---
+++ react-exercises/src/DisplayLanguage.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+import React from "react";
+
+export const LanguageContext = createContext('en');
+
+
+export class DisplayLanguage extends React.Component {
+    render(){
+        return (
+            <div></div>
+        )
+    }
+}
